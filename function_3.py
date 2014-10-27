76319f8cb6337c2b0f0969c9b2802b664f745aba
Grishman <grishman4life@gmail.com>
2014-10-27 17:39:20 +0200

    teste commit

--- function_3.py
+++ function_3.py
@@ -6,6 +6,7 @@ time-and-a-half in a function called computepay() and use the function to do the
  a value. Use 45 hours and a rate of 10.50 per hour to test the program (the pay should be 498.75). You should use
  raw_input to read a string and float() to convert the string to a number. Do not worry about error checking the user
  input unless you want to - you can assume the user types numbers properly.
+ testtestetstte
 '''
 
 
